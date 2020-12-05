e304944cf0db9536b3e15187641154b2b3412641
Marvinho <Marvin.Springer@web.de>
2020-12-05 10:44:19 +0100

    optimize day5 (binary approach)

--- 2020/day5/day5.py
+++ 2020/day5/day5.py
@@ -1,39 +1,24 @@
 with open("day5_input.txt") as f:
 	seats = [line.rstrip() for line in f]
 
-print(seats)
+#print(seats)
 seat_ids = []
 def binaryBoarding(seats):
 	
 	for seat in seats:
-		lower = 0
-		upper = 128
-		lower_col = 0
-		upper_col = 8
-		#print(seat)
-		for char in seat[:7]:
-			#print(char)
-			if(char == "F"):
-				upper = lower+ ((upper-lower) / 2)
-			if(char == "B"):
-				lower = lower + (upper-lower)/2
-			#print(lower, upper)
-		for char in seat[7:]:
+		seat_id = int(seat.replace("F","0").replace("B","1").replace("L","0").replace("R","1"),2)
 
-
-			if(char == "R"):
-				lower_col = lower_col+ ((upper_col-lower_col) / 2)
-			if(char == "L"):
-				upper_col = lower_col+ ((upper_col-lower_col) / 2)
-			#print(lower_col, upper_col)
-
-		seat_ids.append(lower * 8 + lower_col)
+		seat_ids.append(seat_id)
 	return seat_ids
+
 seat_ids = binaryBoarding(seats)
+print(max(seat_ids))
+
 def yourSeatId(seat_ids):
-	print(sorted(seat_ids))
-	print(sum(seat_ids)-sum(range(54,879)))
-	print(sum(seat_ids))
+	#print(sorted(seat_ids))
+	print(sum(range(min(seat_ids),max(seat_ids)+1))-sum(seat_ids))
+	#print(sum(seat_ids))
+
 yourSeatId(seat_ids)
 
 
