897a9596c16d9726d1e05a4eaa3ee6bca643616f
Marvinho <Marvin.Springer@web.de>
2020-12-06 11:53:01 +0100

    part1 set solution

--- 2020/day6/day6.py
+++ 2020/day6/day6.py
@@ -1,42 +1,38 @@
-with open("day6_input.txt") as f:
+with open("test.txt") as f:
 	a = f.read().strip().split("\n\n")
-	b = [x.replace("\n"," ") for x in a]
+	all_answers = [x.replace("\n","") for x in a]
 	#lines = [line.rstrip() for line in f]
-print(b)
+print(all_answers)
 
 form_set = "abcdefghijklmnopqrstuvwxyz"
-def customCustoms(b):
+def customCustoms(all_answers):
 	result = 0
-	for i in b:
-		yes = ""
-		for j in i:
-			if(j in form_set):
-				if(j not in yes):
-					yes+=j
-					result+=1
+	for group_answers in all_answers:
+		set_answers = set("".join(group_answers))
+		result+=len(set_answers)
 	return result
-result = customCustoms(b)
-#print(result)
-
-def part2(b):
-	result = 0
-	for i in b:
-		groups = i.split(" ")
-		i = i.replace(" ","")
-		answers = {}
-		for char in i:
-			if(char in answers):
-				answers[char] += 1
-			else:
-				answers[char] = 1
-		
-		num_groups = len(groups)
-		for k, v in answers.items():
-			if(v == num_groups):
-				result+=1
-		#print(num_groups, answers)
-
-	return result
-
-result = part2(b)
+result = customCustoms(all_answers)
 print(result)
+
+# def part2(b):
+# 	result = 0
+# 	for i in b:
+# 		groups = i.split(" ")
+# 		i = i.replace(" ","")
+# 		answers = {}
+# 		for char in i:
+# 			if(char in answers):
+# 				answers[char] += 1
+# 			else:
+# 				answers[char] = 1
+		
+# 		num_groups = len(groups)
+# 		for k, v in answers.items():
+# 			if(v == num_groups):
+# 				result+=1
+# 		#print(num_groups, answers)
+
+# 	return result
+
+# result = part2(b)
+# print(result)
