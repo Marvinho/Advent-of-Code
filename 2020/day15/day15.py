991487776b11d4251c567e04792776549f77e654
Marvinho <Marvin.Springer@web.de>
2020-12-15 10:57:40 +0100

    add day15 part2

--- 2020/day15/day15.py
+++ 2020/day15/day15.py
@@ -2,13 +2,14 @@ arr = [2,0,6,12,1,3]
 # arr = [0, 3, 6]
 print(arr[len(arr):0:-1].index(arr[-1]))
 print(arr[len(arr):0:-1])
-def part1(input):
+th_number = 30000000
+def parts(arr, th_number):
 	turn_num = 0
 	d = {}
 	for idx, j in enumerate(arr[:-1]):
 		d[j] = idx+1
 	# print(d)
-	for i in range(2020-len(arr)):
+	for i in range(th_number-len(arr)):
 		last_spoken = arr[-1]
 		# print(last_spoken)
 		if(last_spoken in d.keys()):
@@ -17,6 +18,7 @@ def part1(input):
 		else:
 			arr.append(0)
 		d[last_spoken] = len(arr)-1
-	print(arr)
+	print(arr[-1])
 
-part1(input)
+parts(arr, 2020)
+parts(arr, 30000000)
