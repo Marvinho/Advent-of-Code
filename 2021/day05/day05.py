6b3e9d5c2d76d0422b7c13ea3cb4c2ad95b48328
Marvinho <Marvin.Springer@web.de>
2021-12-05 17:12:36 +0100

    day05 part2

--- 2021/day05/day05.py
+++ 2021/day05/day05.py
@@ -16,16 +16,53 @@ def find_vents(line_of_vents):
             for pos in l:
                 diagram[x1][pos]+=1
             # print(l)
-        if(y1==y2):
+        elif(y1==y2):
             l = [x for x in range(min(x1,x2), max(x1,x2)+1)]
             # print(l)
             for pos in l:
                 diagram[pos][y1]+=1
+        elif((x1 > x2) and (y1>y2)):
+            xl = [x for x in range(min(x1,x2), max(x1,x2)+1)][::-1]
+            yl = [y for y in range(min(y1,y2), max(y1,y2)+1)][::-1]
+            for xpos, ypos in zip(xl, yl):
+                diagram[xpos][ypos]+=1            
+        elif(x1 > x2 and y1 < y2):
+            print(x1, y1, x2, y2)
+            # print([x for x in range(min(x1,x2), max(x1,x2)+1)][::-1])
+            xl = [x for x in range(min(x1,x2), max(x1,x2)+1)][::-1]
+            yl = [y for y in range(min(y1,y2), max(y1,y2)+1)]
+            # print(xl, yl)
+            for xpos, ypos in zip(xl, yl):
+                diagram[xpos][ypos]+=1
+        elif(y1 > y2):
+            xl = [x for x in range(min(x1,x2), max(x1,x2)+1)]
+            yl = [y for y in range(min(y1,y2), max(y1,y2)+1)][::-1]
+            for xpos, ypos in zip(xl, yl):
+                diagram[xpos][ypos]+=1
+        else:
+            xl = [x for x in range(min(x1,x2), max(x1,x2)+1)]
+            yl = [y for y in range(min(y1,y2), max(y1,y2)+1)]
+            for xpos, ypos in zip(xl, yl):
+                diagram[xpos][ypos]+=1
         # print(diagram)
     return diagram
 import numpy as np
 diagram = find_vents(line_of_vents)
 print(np.array(diagram).T)
+
+def find_vents_part2(line_of_vents):
+    diagram = [[0 for x in range(10)] for x in range(10)]
+    # print(diagram)
+    for line in line_of_vents:
+        x1, y1, x2, y2 = line
+        xl = [x for x in range(min(x1,x2), max(x1,x2)+1)]
+        yl = [y for y in range(min(y1,y2), max(y1,y2)+1)]
+        for xpos, ypos in zip(xl, yl):
+                diagram[xpos][ypos]+=1
+    return diagram
+import numpy as np
+# diagram = find_vents_part2(line_of_vents)
+# print(np.array(diagram).T)
 
 def calc_score(diagram):
     score = 0
