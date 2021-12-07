0fcabdf145bc642066ee7b11bd0a9adb9270adcf
Marvinho <Marvin.Springer@web.de>
2021-12-07 10:08:04 +0100

    day07 part2

--- 2021/day07/day07.py
+++ 2021/day07/day07.py
@@ -1,5 +1,5 @@
 import math
-with open("testday07.txt") as f:
+with open("day07.txt") as f:
 	for line in f:
 		h_positions = line.strip().split(",")
 		h_positions = [int(x) for x in h_positions]
@@ -10,12 +10,18 @@ def align_horizontally(h_positions):
 	for pos in h_positions:
 		h_pos_dict[pos] += 1
 	print(h_pos_dict)
-	min_fuel_used = sum(h_positions)+1
+	min_fuel_used = 999999999
 	print(min_fuel_used)
 	for i in range(0, max(h_positions)):
 		fuel_used = 0
+
 		for key, value in h_pos_dict.items():
-			fuel_used += abs(i*value - key*value)
+			distance = 0
+			distance += abs(i - key)
+			dist_fuel = 0
+			for j in range(1,distance+1):
+				dist_fuel+= j
+			fuel_used+= dist_fuel*value
 		if(fuel_used < min_fuel_used):
 			min_fuel_used = fuel_used
 			print(fuel_used)
