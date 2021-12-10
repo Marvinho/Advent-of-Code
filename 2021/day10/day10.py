0d977201d1564f157e034f70f0d1a0b5e30797b8
Marvinho <Marvin.Springer@web.de>
2021-12-10 10:45:14 +0100

    day10 part2

--- 2021/day10/day10.py
+++ 2021/day10/day10.py
@@ -3,11 +3,16 @@ with open("day10.txt") as f:
 	for c in f:
 		lines.append(c.strip())
 
-print(lines)
+# print(lines)
 
 def first_illegal_character(lines):
+	score_auto_dict = {"(":1, "[":2, "{":3, "<":4}
+	score = 0
+	scores = []
 	ill_list = []
+	incompletes = []
 	for line in lines:
+		flag = True
 		stack = [line[0]]
 		for bracket in line[1:]:
 			if(bracket in "(<{["):
@@ -19,29 +24,45 @@ def first_illegal_character(lines):
 							stack.pop()
 						else:
 							ill_list.append(bracket)
+							flag = False
 							break
 					case "]":
 						if(stack[-1] == "["):
 							stack.pop()
 						else:
 							ill_list.append(bracket)
+							flag = False
 							break
 					case "}":
 						if(stack[-1] == "{"):
 							stack.pop()
 						else:
 							ill_list.append(bracket)
+							flag = False
 							break
 					case ">":
 						if(stack[-1] == "<"):
 							stack.pop()
 						else:
 							ill_list.append(bracket)
+							flag = False
 							break
-	print(ill_list)
-	return ill_list
+		if(flag):
+			for s in stack[::-1]:
+				score = (score*5)+ score_auto_dict[s]			
+			scores.append(score)
+			score = 0
+			incompletes.append(line)
+			print(scores)
 
-def calc_score(ill_list):
+	# scores = scores.sort()
+	scores.sort()
+	print(scores)
+	print(scores[int(len(scores)/2)])
+	return incompletes, ill_list
+	
+
+def calc_score_part1(ill_list):
 	score_dict = {")": 3, "]": 57, "}": 1197, ">": 25137}
 	score = 0
 	for ill in ill_list:
@@ -50,6 +71,4 @@ def calc_score(ill_list):
 	return score
 
 
-
-ill_list = first_illegal_character(lines)
-calc_score(ill_list)
+incompletes, ill_list = first_illegal_character(lines)
