e86e961a270b470f40d4bb94ddd745d6656a5b9a
Marvinho <Marvin.Springer@web.de>
2021-12-10 16:47:02 +0100

    day09part2

--- 2021/day09/day09.py
+++ 2021/day09/day09.py
@@ -10,6 +10,7 @@ print(heightmap.shape)
 def part1(heightmap):
 	print(heightmap)
 	low_points = []
+	pos_low_points = []
 	row_len = heightmap.shape[1]-1
 	column_len = heightmap.shape[0]-1
 	for i, row in enumerate(heightmap):
@@ -21,15 +22,46 @@ def part1(heightmap):
 					column<heightmap[i-1][j] and
 					column<heightmap[i+1][j] and
 					column<heightmap[i][j+1]):
-					print(i, j)
-					print(column)
+					# print(i, j)
+					# print(column)
 					low_points.append(column)
-	return low_points
+					pos_low_points.append((i, j))
+	return low_points, pos_low_points
+
+
+
+def locate_basins(i, j):
+	print(i, j )
+	
+	score = 0
+	if(heightmap[i][j] == 9 or (i==0 or i==heightmap.shape[0]-1 or 
+								j==0 or j==heightmap.shape[1]-1)):
+		return 0
+	else:
+		heightmap[i][j] = 9
+		score+= 1
+		# locate_basins()
+		print(score)
+		return (score + locate_basins(i, j-1) 
+				+ locate_basins(i-1, j)
+				+ locate_basins(i+1, j)
+				+ locate_basins(i, j+1))
 
 def calc_score (low_points):
 	score = sum(low_points)+len(low_points)
 	print(score)
 	return score
-low_points = part1(heightmap)
-print(low_points)
-calc_score(low_points)
+
+
+# print(low_points)
+# calc_score(low_points)
+
+if __name__=="__main__":
+	basins = []
+	low_points, pos_low_points = part1(heightmap)
+	for i, j in pos_low_points:
+		basins.append(locate_basins(i, j))
+	basins = sorted(basins)
+	result = basins[-1]*basins[-2]*basins[-3]
+	print(result)
+
