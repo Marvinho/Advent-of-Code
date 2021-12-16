8aad4e953d71007a19c1a63333442c64fdc14f55
Marvinho <Marvin.Springer@web.de>
2021-12-16 10:37:18 +0100

    day15part1 astar

--- 2021/day15/day15.py
+++ 2021/day15/day15.py
@@ -1,14 +1,14 @@
 import numpy as np
-with open("testday15.txt") as f:
+with open("day15.txt") as f:
 	risk_levels = []
 	for line in f:
 		risk_levels.append([int(l) for l in line.strip()])
 	# print(risk_levels)
 
 risk_levels = np.array(risk_levels)
-risk_levels = np.pad(risk_levels, pad_width=1, mode='constant', constant_values=-1)
+# risk_levels = np.pad(risk_levels, pad_width=1, mode='constant', constant_values=-1)
 
-print(risk_levels[0][2])
+# print(risk_levels[0][2])
 goal = risk_levels.shape
 leny, lenx = risk_levels.shape[0], risk_levels.shape[1]
 openlist = {(0, 0): (0, 20, 0)}
@@ -20,19 +20,21 @@ closedlist = {}
 ##########################################################
 
 def astar(risk_levels):
-	
+	# print(risk_levels, openlist)
 	while(len(openlist)>0):
-		minimum = (0,0, 999999999)
+		minimum = (0,0,999999999)
 		for key, value in openlist.items():
+			# print(value)
 			if(value[2] < minimum[2]):
 				minimum = value
-				minimum_pos = key
-		currentNode = openlist.pop(minimum_pos)
-		closedlist[minimum_pos] = currentNode
-		print(currentNode)
-		if(minimum_pos == goal):
+				currentNode = key
+		closedlist[currentNode] = openlist[currentNode]
+		del openlist[currentNode]
+		
+		# print(currentNode, (risk_levels.shape[0], risk_levels.shape[1]))
+		if(currentNode == (risk_levels.shape[0]-1, risk_levels.shape[1]-1)):
 			print("path_found")
-			return fhaslkfhdsklfdh
+			print(currentNode, closedlist[currentNode])
 		
 		expandNode(currentNode)
 
@@ -41,21 +43,34 @@ def expandNode(currentNode):
 	posy, posx = currentNode[0], currentNode[1]
 	succ = [(0,1), (1,0), (0,-1), (-1,0)]
 	for i in succ:
-		to_add = (posy+i[0], posx+i[1], risk_levels[posy++i[0]][posx+i[1]])
-		if(to_add[2]==-1):
+		to_add = (currentNode[0]+i[0], currentNode[1]+i[1])
+		if(to_add[0]==-1 or 
+			to_add[1]==-1 or 
+			to_add[0]>=risk_levels.shape[0] or 
+			to_add[1]>=risk_levels.shape[1]):
 			continue
 		else:
-			successors.append(to_add)
+			successors[to_add] = (0, 0, 0)
 	for s in successors:
 		if(s in closedlist):
 			continue
-		s[2] = currentNode[2] + s[2]
-		childh = risk_levels[0]-s[0] + risk_levels[1]-s[1]
+		childg = closedlist[currentNode][0] + risk_levels[s[0]][s[1]]
+		print(s, childg)
+		childh = -2+risk_levels.shape[0] +risk_levels.shape[1] - s[0]-s[1]
 		childf = childh+childg
+		# print(childg, childh, childf)
 		if(s in openlist):
-			if(s[2])
-			continue
-		s[2] = tent_g
+			if(childf > openlist[s][2]):
+				continue
+		if(s in closedlist):
+			if(childf > closedlist[s][2]):
+				continue
 
+		# print(openlist)
+		print(risk_levels[s[0]][s[1]])
+		openlist[s] = (childg, childh, childf)
 
 	# successors = 
+if __name__=="__main__":
+	astar(risk_levels)
+	# print(a, b)
