e5d138f24942845667f84f16510303fc6a190b53
Marvin <marvin.sp@yahoo.com>
2023-12-04 21:50:07 +0100

    day2 working

--- 2023/day01/day01.ts
+++ 2023/day01/day01.ts
@@ -1,42 +1,37 @@
 import * as fs from 'fs';
 
-const data = fs.readFileSync('/home/marvin/Documents/Advent-of-Code/2023/day01/example.txt', 'utf-8');
+const data = fs.readFileSync('/home/marvin/Documents/Advent-of-Code/2023/day01/input.txt', 'utf-8');
 
-function part1() {
+function part1(reg: RegExp) {
   let result = 0;
-  const regex = /\d/g;
-
   let data_arr = data.split('\n');
 
-  data_arr.forEach((element) => {
-    let num = element.match(regex);
+  data_arr.forEach((row) => {
+    let num = row.match(reg);
     if (num) {
       result += Number((num.at(0) ?? '0') + (num.at(-1) ?? '0'));
     }
   });
   console.log(result);
 }
-//part1();
 
-const number_dict = {
-  one: '1',
-  two: '2',
-  three: '3',
-  four: '4',
-  five: '5',
-  six: '6',
-  seven: '7',
-  eight: '8',
-  nine: '9',
-};
-
-function part2() {
+function part2(reg: RegExp) {
+  const number_dict = {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+  };
   let result = 0;
-  const regex = /one|two|three|four|five|six|seven|eight|nine|\d/g;
   let data_arr = data.split('\n');
 
-  data_arr.forEach((element) => {
-    let num: RegExpMatchArray = element.match(regex)!;
+  data_arr.forEach((row) => {
+    let num: string[] = matchOverlapping(row, reg);
     if (num?.at(0) && num.at(-1)) {
       let num1 = isNaN(Number(num.at(0))) ? number_dict[num.at(0) as keyof typeof number_dict] : num?.at(0);
       let num2 = isNaN(Number(num.at(-1))) ? number_dict[num.at(-1) as keyof typeof number_dict] : num?.at(-1);
@@ -46,4 +41,21 @@ function part2() {
     }
   });
 }
-part2();
+
+function matchOverlapping(input: string, reg: RegExp) {
+  const matches: string[] = [];
+  let found: RegExpExecArray | null;
+  while ((found = reg.exec(input))) {
+    matches.push(found[0]);
+    reg.lastIndex -= found[0].length - 1;
+  }
+  return matches;
+}
+
+let reg: RegExp;
+reg = /\d/g;
+part1(reg);
+reg = /one|two|three|four|five|six|seven|eight|nine|\d/g;
+part2(reg);
+let asdf = 'asdf';
+asdf.at(0);
